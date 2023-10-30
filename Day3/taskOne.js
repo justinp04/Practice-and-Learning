be3c8de3f6ef6043f43260fd9da6288018a22923
justinp04 <justin.pan688@gmail.com>
2023-10-30 18:25:06 +0800

    Day 3 completed

--- Day3/taskOne.js
+++ Day3/taskOne.js
@@ -1,2 +1,4 @@
 const now = new Date();
 console.log(now.getFullYear());
+
+console.log(now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
