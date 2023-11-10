cb656e79b4494c821777a977b46bc4f6386a9d4f
justinp04 <justin.pan688@gmail.com>
2023-11-10 23:36:01 +0800

    Started day9

---
+++ Day10/setsAndMaps.js
@@ -0,0 +1,6 @@
+// Sets are a collection of elements and can only caontain unique elements.
+// Sets are iterable and you can iterate through each element
+
+// You can add an element to a set via set.add(). F.e
+const companies = new Set();
+companies.add("Wildcat");
